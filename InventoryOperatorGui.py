e6e3007ae7dec39f94f76ce578b3206bb3ab5a41
Yusuke Manaka <y-manaka@sip-mi.t.u-tokyo.ac.jp>
2018-08-08 14:26:56 +0900

    modified

--- InventoryOperatorGui.py
+++ InventoryOperatorGui.py
@@ -19,7 +19,7 @@ class InventoryOperatorGUI ( wx.Frame ):
 	def __init__( self, parent ):
 		wx.Frame.__init__ ( self, parent, id = wx.ID_ANY, title = u"Inventory Operator", pos = wx.DefaultPosition, size = wx.Size( 697,523 ), style = wx.DEFAULT_FRAME_STYLE|wx.TAB_TRAVERSAL )
 		
-		self.SetSizeHints( wx.DefaultSize, wx.DefaultSize )
+		#self.SetSizeHints( wx.DefaultSize, wx.DefaultSize )
 		
 		bSizer4 = wx.BoxSizer( wx.VERTICAL )
 		
