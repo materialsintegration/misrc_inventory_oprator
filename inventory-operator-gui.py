90a8dc9351bde3583093282e14422330c0f49ee4
Yusuke Manaka <y-manaka@sip-mi.t.u-tokyo.ac.jp>
2018-08-08 14:03:21 +0900

    GUIのクラス名の変更

--- inventory-operator-gui.py
+++ inventory-operator-gui.py
@@ -14,7 +14,7 @@ import wx.xrc
 ## Class MyFrame2
 ###########################################################################
 
-class MyFrame2 ( wx.Frame ):
+class InventoryOperator ( wx.Frame ):
 	
 	def __init__( self, parent ):
 		wx.Frame.__init__ ( self, parent, id = wx.ID_ANY, title = u"Inventory Operator", pos = wx.DefaultPosition, size = wx.Size( 697,523 ), style = wx.DEFAULT_FRAME_STYLE|wx.TAB_TRAVERSAL )
