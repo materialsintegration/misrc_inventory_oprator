fde0c9b189f9b847fef451aa68ac3abf03396934
間中 祐介 <manaka.yusuke@nims.go.jp>
2021-10-04 18:51:44 +0900

    first commit

---
+++ generate_executable_script.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3.6
+# -*- coding: utf-8 -*-
+# Copyright (c) The University of Tokyo and
+# National Institute for Materials Science (NIMS). All rights reserved.
+# This document may not be reproduced or transmitted in any form,
+# in whole or in part, without the express written permission of
+# the copyright owners.
+
+'''
+文献から予測モジュールを作成する際の実行スクリプト作成プログラム
+'''
+
+import sys, os
